3b7c968301f8334024fdbc9f1280b9703b85d6d2
unfuz3 <mr.mendozamen@gmail.com>
2022-09-29 20:30:22 +0200

    Basic Django files. Members app, with basic html response

---
+++ myworld/members/views.py
@@ -0,0 +1,8 @@
+from json import load
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template("myfirst.html")
+    return HttpResponse(template.render())
