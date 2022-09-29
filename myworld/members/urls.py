de15d797b77be921256a845d4c68c84743db8dad
unfuz3 <mr.mendozamen@gmail.com>
2022-09-29 21:10:18 +0200

    Basic app with a member list, add function, and 6 members in db

--- myworld/members/urls.py
+++ myworld/members/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("", views.index, name="index")
+	path("", views.index, name="index"),
+	path("add/", views.add, name="add"),
+	path("add/addrecord/", views.addrecord, name="addrecord")
 ]
--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,8 +1,23 @@
-from json import load
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+from .models import Members
 
 def index(request):
+    mymembers = Members.objects.all().values()
     template = loader.get_template("myfirst.html")
-    return HttpResponse(template.render())
+    context = {
+        "mymembers": mymembers
+    }
+    return HttpResponse(template.render(context, request))
+
+def add(request):
+    template = loader.get_template("add.html")
+    return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+    x = request.POST["first"]
+    y = request.POST["last"]
+    member = Members(firstname=x, lastname=y)
+    member.save()
+    return HttpResponseRedirect(reverse("index"))
